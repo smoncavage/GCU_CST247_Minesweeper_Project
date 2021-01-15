e4e9a67ac573ca97e25e27e06b3272d4db62de7b
Tim James <32233010+tjames222@users.noreply.github.com>
2021-01-14 20:59:52 -0800

    Added UserModel class

---
+++ CLC_MineSweeper/Models/UserModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CLC_MineSweeper.Models
+{
+    public class UserModel
+    {
+        // Properties for the User 
+        private string firstName { get; set; }
+        private string lastName { get; set; }
+        private string sex { get; set; }
+        private int age { get; set; }
+        private string state { get; set; }
+        private string email { get; set; }
+        private string username { get; set; }
+        private string password { get; set; }
+    }
+}
