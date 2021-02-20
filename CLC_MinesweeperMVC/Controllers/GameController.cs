880444a3dd29f3e096a5a9d83f13a6883412dd15
Stephan <moncavage@hotmail.com>
2021-02-20 15:22:01 -0500

    Update game loading

--- CLC_MinesweeperMVC/Controllers/GameController.cs
+++ CLC_MinesweeperMVC/Controllers/GameController.cs
@@ -9,26 +9,14 @@ using System.Web.UI.WebControls;
 
 namespace CLC_MinesweeperMVC.Controllers {
     public class GameController:Controller {
-        public ImageMap flg = new ImageMap();
-        public ImageMap bmb = new ImageMap();
         public static int Difficulty = 1;
-        public static int brdSize = 7;
-        private static List<CellModel> buttons = new List<CellModel>();
-        public bool isWon = false;
-        public CellModel[,] btnGrid = new CellModel[Difficulty*7, Difficulty*7];
-        BoardModel myBoard = new BoardModel(brdSize, Difficulty);
-
+        private GameModel game = new GameModel();
         // GET: Button
         public ActionResult Index(string radioButton) {
-            flg.ImageUrl="~/Images/checkered_flag.bmp";
-            bmb.ImageUrl="~/Images/bomb.bmp";
             Difficulty=Int32.Parse(radioButton);
             ViewBag.message=Difficulty;
-            brdSize=Difficulty*7;
-            myBoard=new BoardModel(brdSize, Difficulty);
-            buttons=myBoard.ConvertGridtoList();
-            ViewBag.ButtonList=buttons;
-            return PartialView("_BoardPage", myBoard);
+            game.Initialize(Difficulty);
+            return PartialView("_BoardPage", game.myBoard);
         }
 
         // GET: Game
@@ -38,46 +26,13 @@ namespace CLC_MinesweeperMVC.Controllers {
 
         [HttpPost]
         public PartialViewResult OnButtonClick(string BoardButtons) {
-            //var buttonValue = ButtonName.;
-            int cnt = int.Parse(BoardButtons);
-            ViewBag.message=Difficulty;
-            for(int rw = 0; rw<brdSize; rw++) {
-                for(int cl = 0; cl<brdSize; cl++) {
-                    //if((sender as Button).Equals(grid[rw, cl])) {
-                    if(myBoard.grid[rw, cl].countValue==buttons[cnt].countValue) {
-
-                        if(myBoard.grid[rw, cl].live) {
-                            myBoard.grid[rw, cl].Image=bmb;
-                            isWon=true;
-                            //MessageBox.Show("You hit a Mine! Length of play was: "+watch.Elapsed);
-                        }
-                        else {
-                            if(myBoard.grid[rw, cl].liveNeighbors==0&&!myBoard.grid[rw, cl].live) {
-                                isWon=false;
-                                //myBoard.FloodFill(rw, cl,1);
-                                //FloodShow(rw, cl);
-                                myBoard.CheckSurround(rw, cl);
-                            }
-                            myBoard.grid[rw, cl].visited=true;
-                        }
-                    }
-
-                }
-            }
-            buttons=myBoard.ConvertGridtoList();
-            ViewBag.ButtonList=buttons;
-            if(isWon) {
-                myBoard.ShowAll();
-                //MessageBox.Show("You Won! Length of play was: "+watch.Elapsed);
-            }
-            myBoard.UpdateButtonLabels();
-            //(sender as Button).BackColor=Color.AliceBlue;
-            return PartialView("_BoardPage", myBoard);
+            game.OnButtonClick(BoardButtons);
+            ViewBag.ButtonList=game.buttons;
+            return PartialView("_BoardPage", game.myBoard);
         }
 
-
         public PartialViewResult BoardPage() {
-            return PartialView("_BoardPage", myBoard); ;
+            return PartialView("_BoardPage", game); ;
         }
 
 
---
+++ CLC_MinesweeperMVC/Models/GameModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+using CLC_MinesweeperMVC.Models;
+
+namespace CLC_MinesweeperMVC.Models {
+    public class GameModel {
+        public ImageMap flg = new ImageMap();
+        public ImageMap bmb = new ImageMap();
+        public static int Difficulty = 1;
+        public static int brdSize = 7;
+        public List<CellModel> buttons = new List<CellModel>();
+        public bool isWon = false;
+        public CellModel[,] btnGrid = new CellModel[Difficulty*7, Difficulty*7];
+        public BoardModel myBoard = new BoardModel(brdSize, Difficulty);
+
+        public GameModel(){}
+
+        public BoardModel Initialize(int difficulty){
+            flg.ImageUrl="~/Images/checkered_flag.bmp";
+            bmb.ImageUrl="~/Images/bomb.bmp";
+            brdSize=Difficulty*7;
+            if(myBoard.inPlay==false) {
+                myBoard=new BoardModel(brdSize, Difficulty);
+                
+                //ViewBag.ButtonList=buttons;
+            }
+            buttons=myBoard.ConvertGridtoList();
+            return myBoard;
+        }
+
+        public BoardModel OnButtonClick(string BoardButtons) {
+            //var buttonValue = ButtonName.;
+            int cnt = int.Parse(BoardButtons);
+            //ViewBag.message=Difficulty;
+            for(int rw = 0; rw<brdSize; rw++) {
+                for(int cl = 0; cl<brdSize; cl++) {
+                    //if((sender as Button).Equals(grid[rw, cl])) {
+                    if(myBoard.grid[rw, cl].countValue==buttons[cnt].countValue) {
+
+                        if(myBoard.grid[rw, cl].live) {
+                            myBoard.grid[rw, cl].Image=bmb;
+                            isWon=true;
+                            //MessageBox.Show("You hit a Mine! Length of play was: "+watch.Elapsed);
+                        }
+                        else {
+                            if(myBoard.grid[rw, cl].liveNeighbors==0&&!myBoard.grid[rw, cl].live) {
+                                isWon=false;
+                                //myBoard.FloodFill(rw, cl,1);
+                                //FloodShow(rw, cl);
+                                myBoard.CheckSurround(rw, cl);
+                            }
+                            myBoard.grid[rw, cl].visited=true;
+                        }
+                    }
+
+                }
+            }
+            buttons=myBoard.ConvertGridtoList();
+            //ViewBag.ButtonList=buttons;
+            if(isWon) {
+                myBoard.ShowAll();
+                //MessageBox.Show("You Won! Length of play was: "+watch.Elapsed);
+            }
+            myBoard.UpdateButtonLabels();
+            //(sender as Button).BackColor=Color.AliceBlue;
+            return myBoard;
+        }
+    }
+}
