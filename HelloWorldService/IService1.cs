33231b0e5bf3b24c987f2af06f40b72f950217ee
Stephan <moncavage@hotmail.com>
2021-02-21 10:07:07 -0500

    Update Presentation For Final Submission.

---
+++ HelloWorldService/IService1.cs
@@ -0,0 +1,29 @@
+﻿using javax.management.openmbean;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace HelloWorldService {
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1 {
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "SayHello/")]
+        string SayHello();
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "GetData/{value}")]
+        string GetData(string value);
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "GetObjectModel/{id}")]
+        string GetObjectModel(string id);
+    }
+
+
+    
+}
---
+++ HelloWorldService/Service1.svc.cs
@@ -0,0 +1,33 @@
+﻿using javax.management.openmbean;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace HelloWorldService {
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1:IService1 {
+        
+        public string GetData(string value) {
+            return "You entered: "+value;
+        }
+
+        public string GetObjectModel(string id) {
+            if(id==null) {
+                throw new ArgumentNullException("ID");
+                
+            }
+            
+            return id;
+        }
+
+        public string SayHello() {
+            throw new NotImplementedException();
+        }
+
+    }
+}
